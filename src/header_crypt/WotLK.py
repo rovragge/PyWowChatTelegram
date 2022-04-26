a6b30a5d3b4b7bd97befac18fefba4f90e82911b
Daniil <35235879+Anarom@users.noreply.github.com>
2022-04-27 02:19:53 +0300

    Correct usage of hmac

--- src/header_crypt/WotLK.py
+++ src/header_crypt/WotLK.py
@@ -25,17 +25,21 @@ class GameHeaderCrypt(Vanilla.GameHeaderCrypt):
         if not self.initialized:
             config.cfg.logger.debug('encrypt method called on uninitialized GameHeaderCrypt')
             return btarr
-        self.client_crypt.crypt_to_bytearray(btarr)
+        return self.client_crypt.crypt_to_bytearray(btarr)
 
     def initialize(self, key):
-        md = hmac.new(key, GameHeaderCrypt.SERVER_HMAC_SEED, 'sha1')
-        md.update(key)
+        md = hmac.new(GameHeaderCrypt.SERVER_HMAC_SEED, key, 'sha1')
         server_key = md.digest()
-        md = hmac.new(key, GameHeaderCrypt.CLIENT_HMAC_SEED, 'sha1')
-        md.update(key)
+        md = hmac.new(GameHeaderCrypt.CLIENT_HMAC_SEED, key, 'sha1')
         client_key = md.digest()
         self.server_crypt = RC4(server_key)
-        self.server_crypt.crypt_to_bytearray(bytearray(1024))
         self.client_crypt = RC4(client_key)
+        config.cfg.logger.debug('Header crypt values (big endianness):\n\t'
+                                f'Session key = {int.from_bytes(key, "big")}\n\t'
+                                f'Server key = {int.from_bytes(server_key, "big")}\n\t'
+                                f'Client key = {int.from_bytes(client_key, "big")}\n\t'
+                                f'Server RC4 Sbox head = {self.server_crypt.sbox[:5]}\n\t'
+                                f'Client RC4 Sbox head = {self.client_crypt.sbox[:5]}')
+        self.server_crypt.crypt_to_bytearray(bytearray(1024))
         self.client_crypt.crypt_to_bytearray(bytearray(1024))
         self.initialized = True

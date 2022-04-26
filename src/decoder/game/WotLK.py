d8a4f403a98af69e3ca486e3dcea69a1f1e1416f
Daniil <35235879+Anarom@users.noreply.github.com>
2022-04-27 02:32:55 +0300

    Tidied up

--- src/decoder/game/WotLK.py
+++ src/decoder/game/WotLK.py
@@ -7,11 +7,11 @@ class GamePacketDecoder(Vanilla.GamePacketDecoder):
         header = int.to_bytes(buff.get(GamePacketDecoder.HEADER_LENGTH), GamePacketDecoder.HEADER_LENGTH, 'big')
         decrypted = cfg.crypt.decrypt(header)
 
-        if decrypted[0] & 0x80 == 0x80:
+        if decrypted[0] & 128 == 128:
             next_byte = cfg.crypt.decrypt(int.to_bytes(buff.get(1), 1, 'big'))[0]
             size = (((decrypted[0] & 127) << 16) | ((decrypted[1] & 255) << 8) | (decrypted[2] & 255)) - 2
-            header_id = (next_byte & 255) << 8 | decrypted[3] & 255
+            packet_id = (next_byte & 255) << 8 | decrypted[3] & 255
         else:
             size = ((decrypted[0] & 255) << 8 | decrypted[1] & 255) - 2
-            header_id = (decrypted[3] & 255) << 8 | decrypted[2] & 255
-        return header_id, size
+            packet_id = (decrypted[3] & 255) << 8 | decrypted[2] & 255
+        return packet_id, size

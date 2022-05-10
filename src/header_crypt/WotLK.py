8324855dbddbdb29e909d2f5278717859973f166
Daniil <35235879+Anarom@users.noreply.github.com>
2022-05-10 23:32:19 +0300

    Use arc4 module to speed up de/en coding of headers

--- src/header_crypt/WotLK.py
+++ src/header_crypt/WotLK.py
@@ -1,7 +1,7 @@
 import hmac
+from arc4 import ARC4
 
 import src.common.config as config
-from src.common.RC4 import RC4
 from src.header_crypt import Vanilla
 
 
@@ -18,27 +18,21 @@ class GameHeaderCrypt(Vanilla.GameHeaderCrypt):
         if not self.initialized:
             config.cfg.logger.debug('decrypt method called on uninitialized GameHeaderCrypt')
             return btarr
-        return self.server_crypt.crypt_to_bytearray(btarr)
+        return self.server_crypt.decrypt(bytes(btarr))
 
     def encrypt(self, btarr):
         if not self.initialized:
             config.cfg.logger.debug('encrypt method called on uninitialized GameHeaderCrypt')
             return btarr
-        return self.client_crypt.crypt_to_bytearray(btarr)
+        return self.client_crypt.encrypt(bytes(btarr))
 
     def initialize(self, key):
         md = hmac.new(GameHeaderCrypt.SERVER_HMAC_SEED, key, 'sha1')
         server_key = md.digest()
         md = hmac.new(GameHeaderCrypt.CLIENT_HMAC_SEED, key, 'sha1')
         client_key = md.digest()
-        self.server_crypt = RC4(server_key)
-        self.client_crypt = RC4(client_key)
-        config.cfg.logger.debug('Header crypt values (big endianness):\n\t'
-                                f'Session key = {int.from_bytes(key, "big")}\n\t'
-                                f'Server key = {int.from_bytes(server_key, "big")}\n\t'
-                                f'Client key = {int.from_bytes(client_key, "big")}\n\t'
-                                f'Server RC4 Sbox head = {self.server_crypt.sbox[:5]}\n\t'
-                                f'Client RC4 Sbox head = {self.client_crypt.sbox[:5]}')
-        self.server_crypt.crypt_to_bytearray(bytearray(1024))
-        self.client_crypt.crypt_to_bytearray(bytearray(1024))
+        self.server_crypt = ARC4(server_key)
+        self.client_crypt = ARC4(client_key)
+        self.server_crypt.encrypt(bytes(bytearray(1024)))
+        self.client_crypt.decrypt(bytes(bytearray(1024)))
         self.initialized = True
